7d37e0a826ff3e2e08d24fcb527346813391b56e
Prasangeet <142200325+prasangeet@users.noreply.github.com>
2025-12-28 14:00:33 +0530

    Delete .next/dev directory

--- .next/dev/server/chunks/ssr/_next-internal_server_app_(marketing)_dashboard_page_actions_98d42842.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/(marketing)/dashboard/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_%28marketing%29_dashboard_page_actions_98d42842.js.map
--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"cTJSgyiX81qiJBESmRCVc+azIY1h/keHioBAgdrNb7k=\"\n}"
